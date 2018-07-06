49dd788ed44ff90a4b06e3ffce9486f32afca609
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-06 01:42:15 +0100

    feat: updated service worker functionality; chore: updated readme

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 ---
 #### _Three Stage Course Material Project - Restaurant Reviews_
 
-## Project Overview: Stage 1
+## Project Overview:
 
-For the **Restaurant Reviews** projects, you will incrementally convert a static webpage to a mobile-ready web application. In **Stage One**, you will take a static design that lacks accessibility and convert the design to be responsive on different sized displays and accessible for screen reader use. You will also add a service worker to begin the process of creating a seamless offline experience for your users.
+- implemented ARIA and accesibility features
+- created a fully responsive app using css grid (which btw is awesome)
+- added full online experience using service worker and cache API
 
-### Specification
-
-You have been provided the code for a restaurant reviews website. The code has a lot of issues. It’s barely usable on a desktop browser, much less a mobile device. It also doesn’t include any standard accessibility features, and it doesn’t work offline at all. Your job is to update the code to resolve these issues while still maintaining the included functionality.
-
-### What do I do from here?
+### How to run this on localhost?
 
 1. In this folder, start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this, and you don't even need to know Python. For most people, it's already installed on your computer.
 
@@ -19,16 +17,13 @@ In a terminal, check the version of Python you have: `python -V`. If you have Py
 2. With your server running, visit the site: `http://localhost:8000`, and look around for a bit to see what the current experience looks like.
 3. Explore the provided code, and start making a plan to implement the required features in three areas: responsive design, accessibility and offline use.
 4. Write code to implement the updates to get this site on its way to being a mobile-ready website.
-OR
-Just npm install -g local-web-server then in console type 'ws' and make sure to update you local address in dbhelper.js
+**OR**
+**THE SIMPLE WAY**
+1. Download and install [Node JS](http://nodejs.org/)
+2. Test that node is installed with `node -v` than test npm is installed with `npm -v`
+2. Run npm install -g local-web-server then in you cli  type 'ws' while in the app root directory.
+Oh almost forgot ->  make sure to update you local address in dbhelper.js acordingly
 
 ## Leaflet.js and Mapbox:
 
 This repository uses [leafletjs](https://leafletjs.com/) with [Mapbox](https://www.mapbox.com/). You need to replace `<your MAPBOX API KEY HERE>` with a token from [Mapbox](https://www.mapbox.com/). Mapbox is free to use, and does not require any payment information.
-
-### Note about ES6
-
-Most of the code in this project has been written to the ES6 JavaScript specification for compatibility with modern web browsers and future proofing JavaScript code. As much as possible, try to maintain use of ES6 in any additional JavaScript you write.
-
-
-
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,41 @@
+// Register service worker and print scope
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js', {scope: './'})
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+    if (!navigator.serviceWorker.controller) {
+        return;
+    }
+    if (reg.waiting) {
+        navigator.serviceWorker.controller.postMessage({ action: 'skipWaiting' });
+    }
+    if (reg.installing) {
+        navigator.serviceWorker.addEventListener('statechange', function () {
+            if (navigator.serviceWorker.controller.state == 'installed') {
+                navigator.serviceWorker.controller.postMessage({ action: 'skipWaiting' });
+            }
+        });
+    }
+    reg.addEventListener('updatefound', function () {
+        navigator.serviceWorker.addEventListener('statechange', function () {
+            if (navigator.serviceWorker.controller.state == 'installed') {
+                navigator.serviceWorker.controller.postMessage({ action: 'skipWaiting' });
+            }
+        });
+    });
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 // Create cache for the selected items
+let random = Math.floor(Math.random() * Math.floor(99));
+let cacheData = 'r-cache-'+random;
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open(cacheData).then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
@@ -16,27 +50,42 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// Clone response to cache and send response to browser, if page is not cached serve a 404 image
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    console.log(event.request);
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        let responseClone = response.clone();
-        caches.open('v1').then(function(cache) {
-          cache.put(event.request, responseClone);
-        });
-
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/404.jpg');
-    })
-  );
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('r-cache-') && cacheName != cacheData;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
 });
 
-// self.addEventListener('message', function(event) {
-//   if (event.data.action === 'skipWaiting') {
-//     self.skipWaiting();
-//   }
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+		caches.match(event.request).then(response => {
+			if (response) {
+				console.log(event.request.url);
+				return response;
+			}
+			return fetch(event.request).then(networkResponse => {
+				return caches.open(cacheData).then(cache => {
+					cache.put(event.request.url, networkResponse.clone());
+					console.log(event.request.url);
+					return networkResponse;
+				})
+			})
+		}).catch(function() {
+        return new Response("I'm Sorry But There's Nothing Here !");
+    })
+	);
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
